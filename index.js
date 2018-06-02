dff34a8342edeffabd83cb97ea5db540a4048b65
Deivid Teixeira <deividbatfish2@gmail.com>
2018-06-02 11:44:14 -0300

    Altera a estrutura de models

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const clear = require('clear')
 const chalk = require('chalk')
 const figlet = require('figlet')
 
+const structure = require('./lib/structure/structure')
+
+clear()
+
 console.log(
     chalk.green(
         figlet.textSync('ProtractorCli', { horizontalLayout: 'full' })
     )
 )
+
+const run = () => {
+    structure.createStructureFolders()
+}
+
+run()
